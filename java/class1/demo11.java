539d2680dcf544f7c90a4b7043dcf645fd958b0d
codegod64 <amitabhsingh128@gmail.com>
2025-06-09 10:51:05 +0530

    first day

---
+++ java/class1/demo11.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class demo{
+	public static void main(String[] Args){
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.print(a+b);
+	}
+}
---
+++ java/class1/demo12.java
@@ -0,0 +1,5 @@
+public class demo12{
+	public static void main(String[] Args){
+		System.out.print("hello world");
+	}
+}
