369180dff43fe3f3bb8d0616810bb6bd78432ae9
amitabhsingh64 <amitabhsingh128@gmail.com>
2025-06-10 00:35:54 +0530

    " 9_june night"

---
+++ Chai_js/01_basics/datatypes.js
@@ -0,0 +1,16 @@
+//numbers => 2 to power 53
+//bigint 
+//string =>" "
+//boolean=>true/false
+//null=> standalone value
+//undefined=> not defined ,(not yet assigned)
+//symbol =>unique
+
+
+
+//non-premetive
+
+//object
+
+// null is of object type
+// undefined is of undefined type
---
+++ Chai_js/01_basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "amitabh@google.com"
+var accountPass = "12345"
+accountCity = "Agra"
+
+console.table([accountId, accountEmail, accountPass, accountCity])
