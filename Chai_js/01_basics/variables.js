7e72307d59eee66c3244a04179520f745cb20400
amitabhsingh64 <amitabhsingh128@gmail.com>
2025-06-17 12:12:10 +0530

    Delete Chai_js directory
    removed unnecessary files

--- Chai_js/01_basics/variables.js
+++
@@ -1,6 +0,0 @@
-const accountId = 144553
-let accountEmail = "amitabh@google.com"
-var accountPass = "12345"
-accountCity = "Agra"
-
-console.table([accountId, accountEmail, accountPass, accountCity])
