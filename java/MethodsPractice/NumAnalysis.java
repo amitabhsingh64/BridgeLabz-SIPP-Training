22b57bce8c7b844bf61c02eee582107ec210035f
ShobhitChaturvedi <shobhitchaturvedi14@gmail.com>
2025-06-17 10:42:47 +0530

    amitabh add.num analyser java implementation

---
+++ java/MethodsPractice/NumAnalysis.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class NumAnalysis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+        System.out.println("The largest number is: " + findLargest(num1, num2, num3));
+        System.out.println("The smallest number is: " + findSmallest(num1, num2, num3));
+    }
+    public static int findLargest(int num1, int num2, int num3) {
+        int largest = num1;
+        if (num2 > largest) {
+            largest = num2;
+        }
+        if (num3 > largest) {
+            largest = num3;
+        }
+        return largest;
+    }
+    public static int findSmallest(int num1, int num2, int num3) {
+        int smallest = num1;
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+        if (num3 < smallest) {
+            smallest = num3;
+        }
+        return smallest;
+    }
+}
