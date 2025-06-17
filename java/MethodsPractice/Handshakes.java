391cfc235d8cb6ac70ce10c41dd55dde015e178d
ShobhitChaturvedi <shobhitchaturvedi14@gmail.com>
2025-06-17 10:49:17 +0530

    amitabh add.number of handshakes java implementation

---
+++ java/MethodsPractice/Handshakes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Handshakes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfPeople = sc.nextInt();
+        System.out.println("Total handshakes: " + calculateHandshakes(numberOfPeople));
+    }
+    public static int calculateHandshakes(int numberOfPeople) {
+        if (numberOfPeople < 0) {
+            System.out.println("Number of people cannot be negative.");
+            return 0;
+        }
+        // Using the formula n(n-1)/2 to calculate handshakes
+        return (numberOfPeople * (numberOfPeople - 1)) / 2;
+    }
+}
