2f51aa9213c547fd8f92bf0dd085e4515fb226a5
ShobhitChaturvedi <shobhitchaturvedi14@gmail.com>
2025-06-17 11:19:05 +0530

    amitabh refactor.Quadratic solver discriminant renamed to delta java implementation

--- java/MethodsPractice/QuadraticRoots.java
+++ java/MethodsPractice/QuadraticRoots.java
@@ -14,12 +14,12 @@ public class QuadraticRoots {
             System.out.println("Coefficient 'a' can not be zero for a quadratic equation.");
             return;
         }
-        double discriminant = b * b - 4 * a * c;
-        if (discriminant > 0) {
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        double delta = b * b - 4 * a * c;
+        if (delta > 0) {
+            double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double root2 = (-b - Math.sqrt(delta)) / (2 * a);
             System.out.printf("real and different", root1, root2);
-        } else if (discriminant == 0) {
+        } else if (delta == 0) {
             double root = -b / (2 * a);
             System.out.printf("real and same", root);
         } else {
